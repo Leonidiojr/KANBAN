03358863c05a098a9ae24293ea34555aad261c9e
leonidiojr <leonidiojr@gmail.com>
2023-11-16 20:13:24 -0300

    Primeiro Commit

---
+++ src/main/java/model/entities/Acao.java
@@ -0,0 +1,19 @@
+package model.entities;
+
+import java.util.Date;
+import model.entities.enums.Status;
+
+
+public class Acao {
+    
+    private String nome;
+    private Date dataInicio;
+    private Date dataFim;
+    private Double percentual;
+    private Status Status;
+    private String Responsavel;
+
+
+    
+    
+}
